ec5bf03a70b2d83fc921f2fd3f9beba343327542
frezer takele <129789211+fretak1@users.noreply.github.com>
2025-08-02 17:06:25 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,60 +2,43 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// your first API endpoint...
+app.get('/api/whoami', (req, res) => {
+  const ip = req.ip || req.connection.remoteAddress;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-// Allow optional date param
-app.get('/api/:date?', (req, res) => {
-  let dateString = req.params.date;
-
-  let date;
-  if (!dateString) {
-    // If no date is provided, use the current date
-    date = new Date();
-  } else if (!isNaN(dateString)) {
-    // If dateString is a timestamp (e.g. 1451001600000)
-    date = new Date(parseInt(dateString));
-  } else {
-    // Otherwise try to parse as a date string (e.g. "2015-12-25")
-    date = new Date(dateString);
-  }
-
-  // Check if the date is valid
-  if (date.toString() === "Invalid Date") {
-    return res.json({ error: "Invalid Date" });
-  }
-
-  // Return JSON response
   res.json({
-    unix: date.getTime(),
-    utc: date.toUTCString()
+    ipaddress: ip,
+    language: language,
+    software: software
   });
 });
 
+app.get('/api/whoami', function (req, res) {
+  res.json({ ipaddress : 'hello API' });
+});
 
 
-// Listen on port set in environment variable or default to 3000
+
+// listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
